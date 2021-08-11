2babba42997ee15a8042cb5e34c30238310b6d7f
annabelleerskine <annabelle.erskine@icloud.com>
2021-08-11 15:49:04 +1000

    Initial commit

---
+++ Tutorial.R
@@ -0,0 +1,21 @@
+#Github tutorial w Romain
+
+library(usethis)
+library(gitcreds)
+
+#start
+#why use github? version control software! Also makes accessing code from different computers easy. 
+#git does not have a user interface. Going to type comments in the terminal...
+
+#installing git. Check to see if you have it first. Open terminal in R studio! Type "which git" in terminal to show pathway to where git is.
+#means you have git installed and r knows where to find it. Now going to use r studio to edit git profile. WHen you track change and save it in git, will be associated with your name and email address.
+#type shell in terminal, type r in console.
+#usethis::edit_git_config() --> this will allow me to edit my name and email. (git credentials)
+#now want to enable git in r studio for the first time. Create a new project!! (done, Github_tutorial)
+
+#usethis::use_git()
+
+
+
+
+
