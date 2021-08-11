edf5e6a3b69dd47693c6601c6283c93a7f3631ff
annabelleerskine <annabelle.erskine@icloud.com>
2021-08-11 16:03:19 +1000

    commit3

--- test.R
+++ test.R
@@ -1,2 +1,3 @@
 #type stuff: so needed to then click commit tab, and go into it and click stage and commit (after writing a comment)
-#type again: now it appears blue.
+#type again: now it appears blue. Click on commit and then do the same thing as before.
+
